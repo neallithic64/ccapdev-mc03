d9cda8c6e391e742fcc2e040061d83e536c3bcb6
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2020-04-09 15:03:38 +0800

    Cleaned up code and removed TODOs

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,27 +7,14 @@ const controller = {
         res.status(204);
     },
 
-    /*
-    TODO:   This function is executed when the client sends an HTTP GET
-            request to path `/`. This displays `home.hbs` with all contacts
-            current stored in the database.
-    */
     getIndex: function(req, res) {
         db.findMany(User, {}, '', function(result) {
             res.render('home', {
                 contacts: result
             });
         });
-        // res.render('home'); // This is to load the page initially
     },
 
-    /*
-    TODO:   This function is executed when the client sends an HTTP GET
-            request to path `/getCheckNumber`. This function checks if a
-            specific number is stored in the database. If the number is
-            stored in the database, it returns an object containing the
-            number, otherwise, it returns an empty string.
-    */
     getCheckNumber: function(req, res) {
         var numInput = req.query.number;
         
@@ -37,15 +24,9 @@ const controller = {
         });
     },
 
-    /*
-    TODO:   This function is executed when the client sends an HTTP GET
-            request to path `/getAdd`. This function adds the contact sent
-            by the client to the database, then appends the new contact to the
-            list of contacts in `home.hbs`.
-    */
     getAdd: function(req, res) {
-        var name = req.query.name;
-        var num = req.query.number;
+        var name = req.query.name,
+            num = req.query.number;
 
         db.insertOne(User, {name: name, number: num}, function(result) {
             if (result) {
@@ -56,12 +37,6 @@ const controller = {
         });
     },
 
-    /*
-    TODO:   This function is executed when the client sends an HTTP GET
-            request to path `/getDelete`. This function deletes the contact
-            from the database, then removes the contact to the list of
-            contacts in `home.hbs`.
-    */
     getDelete: function (req, res) {
         var num = req.query.number;
 
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,11 +1,6 @@
 
 var mongoose = require('mongoose');
 
-/*
-    TODO:   Complete the UserSchema which will contain the name and the
-            number of contacts in the database.
-*/
-
 var UserSchema = new mongoose.Schema({
     name: String,
     number: Number
